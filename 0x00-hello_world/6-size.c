a495594fdd42c1f766c5a838920f6db99bf63a6d
ofentsemaleka <ofentsemaleka6@gmail.com>
2023-08-10 06:13:22 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Write a C program that prints the size of various types
+ * return 0
+ */
+
+init main (void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+printf("Size of a char:%lu byte(s)"\n,
