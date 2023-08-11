a09502eb449ec01328776aa2cddb9eae43339039
ofentsemaleka <ofentsemaleka6@gmail.com>
2023-08-11 06:46:51 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main
+ * return: 0
+ */
+
+int main(void)
+{
+        int i;
+        for (i = 0; i < 10; i++)
+                putchar('0' + i);
+        putchar('\n');
+        return 0;
+}
