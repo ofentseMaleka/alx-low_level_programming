7750a90c0636ed6633e330bbfb9d9274c025fbc9
ofentsemaleka <ofentsemaleka6@gmail.com>
2023-08-15 14:37:37 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - x10
+ * return:void
+ */
+
+void print_alphabet_x10(void)
+{
+char ch;
+int i = 0;
+
+while (i < 10)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+i++;
+}
+
+}
+
