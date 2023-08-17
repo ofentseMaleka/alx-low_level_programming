3071b125c504bc7753452c139178487f3e02d346
ofentsemaleka <ofentsemaleka6@gmail.com>
2023-08-17 06:21:25 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line -lines
+ * @n: number of times
+ * return: 0 for success
+ */
+
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
