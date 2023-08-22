158249c261abd10c1c6679e32fe3ca5541bc664f
ofentsemaleka <ofentsemaleka6@gmail.com>
2023-08-22 12:51:08 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-/*
- * main - a code that returns 0
- * Return: 0
- */
-
-init main(void)
-
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return:(0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
